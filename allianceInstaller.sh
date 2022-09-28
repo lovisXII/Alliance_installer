c8ae285e313781f8c6e8ee89ca1ed44312ce31dc
LovisXII <logeofpit@yahoo.fr>
2022-09-28 16:26:32 +0200

    adding env var to avoid error with boog

--- allianceInstaller.sh
+++ allianceInstaller.sh
@@ -3,7 +3,7 @@
 echo "This script will recompile alliance binary for your system"
 echo "Cloning alliance repository in ${HOME}/alliance"
 
-mkdir $HOME/alliance
+mkdir -p $HOME/alliance
 export INSTALL_ALLIANCE_PATH=$HOME/alliance
 cd $INSTALL_ALLIANCE_PATH
 git clone https://gitlab.lip6.fr/vlsi-eda/alliance.git
@@ -29,4 +29,9 @@ ${srcDir}/configure --prefix=${ALLIANCE_TOP} --enable-alc-shared
 make -j1 install
 
 echo "export PATH=$PATH:$INSTALL_ALLIANCE_PATH/install/bin" >> ~/.bashrc
+echo "
+export MBK_IN_LO=vst
+export MBK_OUT_LO=vst
+export MBK_TARGET_LIB=${installDir}/cells/sxlib
+" >> ~/.bashrc
 source ~/.bashrc
